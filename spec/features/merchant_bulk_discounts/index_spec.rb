89d32cea9ae52d3c6c41743910b4f2a50e557271
Antonio King <antonio.j.king@gmail.com>
2021-08-10 08:59:59 -0600

    Create test for bd_ index link to delete discount

--- spec/features/merchant_bulk_discounts/index_spec.rb
+++ spec/features/merchant_bulk_discounts/index_spec.rb
@@ -73,4 +73,34 @@ RSpec.describe 'Merchant Bulk Discounts Index Page' do
 
     expect(current_path).to eq(new_merchant_bulk_discount_path(@merchant1.id))
   end
+
+  # As a merchant
+  # When I visit my bulk discounts index
+  # Then next to each bulk discount I see a link to delete it
+  # When I click this link
+  # Then I am redirected back to the bulk discounts index page
+  # And I no longer see the discount listed
+  it 'has a link to delete a bulk discount and can delete the discount' do
+    visit merchant_bulk_discounts_path(@merchant1.id)
+
+    within("#bd-#{@bulk_discount1.id}") do
+      expect(page).to have_link("Delete this Discount")
+    end
+
+    within("#bd-#{@bulk_discount2.id}") do
+      expect(page).to have_link("Delete this Discount")
+    end
+
+    within("#bd-#{@bulk_discount3.id}") do
+      expect(page).to have_link("Delete this Discount")
+    end
+
+    within("#bd-#{@bulk_discount1.id}") do
+      click_link("Delete this Discount")
+    end
+
+    expect(page).to_not have_link("#{@bulk_discount1.id}")
+    expect(page).to_not have_content(@bulk_discount1.percentage_discount)
+    expect(page).to_not have_content(@bulk_discount1.quantity_threshold)
+  end
 end
