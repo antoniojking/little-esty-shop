ecd4fa6fcb9797690f78bdbe2650d0d6bc216203
Antonio King <antonio.j.king@gmail.com>
2021-08-10 17:38:01 -0600

    Create model tests for inv tot/discounted rev

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -16,11 +16,24 @@ RSpec.describe Invoice do
   end
 
   it 'calculates invoice total revenue' do
-    expect(@invoice1.invoice_items.count).to eq(3)
-    expect(@invoice1.total_revenue).to eq(78750)
+    invoice_item22 = InvoiceItem.create!(invoice_id: @invoice1.id, item_id: @item5.id, quantity: 1, unit_price: @item5.unit_price, status: 'shipped')
+    invoice_item23 = InvoiceItem.create!(invoice_id: @invoice1.id, item_id: @item17.id, quantity: 10, unit_price: @item17.unit_price, status: 'shipped')
+
+    # expect(@invoice1.invoice_items.count).to eq(3)
+    # expect(@invoice1.total_revenue).to eq(78750)
+    expect(@invoice1.invoice_items.count).to eq(5)
+    expect(@invoice1.total_revenue).to eq(25000)
   end
 
   it 'returns list of invoices from old to new with invoice_items that have not been shipped' do
     expect(Invoice.incomplete_invoices_by_date.length).to eq(13)
+  end
+
+  it 'calculates invoice discounted revenue' do
+    invoice_item22 = InvoiceItem.create!(invoice_id: @invoice1.id, item_id: @item5.id, quantity: 1, unit_price: @item5.unit_price, status: 'shipped')
+    invoice_item23 = InvoiceItem.create!(invoice_id: @invoice1.id, item_id: @item17.id, quantity: 10, unit_price: @item17.unit_price, status: 'shipped')
+
+    expect(@invoice1.invoice_items.count).to eq(5)
+    expect(@invoice1.discounted_revenue).to eq(57562.5)
   end
 end
