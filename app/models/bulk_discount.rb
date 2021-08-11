a19848afc0fbc41a83b9f7e3031e514d35e31308
Antonio King <antonio.j.king@gmail.com>
2021-08-11 01:32:40 -0600

    Add validations for bulk discounts

--- app/models/bulk_discount.rb
+++ app/models/bulk_discount.rb
@@ -1,3 +1,10 @@
 class BulkDiscount < ApplicationRecord
   belongs_to :merchant
+
+  validates :percentage_discount, presence: true
+  validates :percentage_discount, numericality: { only_integer: true }
+  validates :percentage_discount, length: { maximum: 99 }
+
+  validates :quantity_threshold, presence: true
+  validates :quantity_threshold, numericality: { only_integer: true }
 end
--- spec/models/bulk_discounts_spec.rb
+++ spec/models/bulk_discounts_spec.rb
@@ -4,4 +4,13 @@ RSpec.describe BulkDiscount do
   describe 'relationships' do
     it { should belong_to(:merchant) }
   end
+
+  describe 'validations' do
+    it { should validate_presence_of(:percentage_discount) }
+    it { should validate_numericality_of(:percentage_discount).only_integer }
+    # it { should validate_length_of(:percentage_discount).is_at_most(99) }
+
+    it { should validate_presence_of(:quantity_threshold) }
+    it { should validate_numericality_of(:quantity_threshold).only_integer }
+  end
 end
