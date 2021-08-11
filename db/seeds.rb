36ef5dbfb20822c5643507e6c939b853f5761aa7
Antonio King <antonio.j.king@gmail.com>
2021-08-11 01:45:14 -0600

    Add seeds data for bulk discounts

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+BulkDiscount.destroy_all
+
+BulkDiscount.create!(percentage_discount: 20, quantity_threshold: 10, merchant_id: 1)
+BulkDiscount.create!(percentage_discount: 30, quantity_threshold: 15, merchant_id: 1)
+BulkDiscount.create!(percentage_discount: 15, quantity_threshold: 15, merchant_id: 1)
+BulkDiscount.create!(percentage_discount: 40, quantity_threshold: 100, merchant_id: 1)
+BulkDiscount.create!(percentage_discount: 35, quantity_threshold: 50, merchant_id: 2)
+BulkDiscount.create!(percentage_discount: 15, quantity_threshold: 5, merchant_id: 2)
+BulkDiscount.create!(percentage_discount: 20, quantity_threshold: 10, merchant_id: 3)
